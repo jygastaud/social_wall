df76de4515284484cc2e129ddb09885c2c851d69
Paul-Émile MINY <paulemile.miny@gmail.com>
2015-07-03 12:15:05 +0200

    wip

--- social_wall.api.php
+++ social_wall.api.php
@@ -5,7 +5,9 @@
  * Hooks provided by the Social wall module.
  */
 
-/**
- * @addtogroup hooks
- * @{
- */
+function hook_social_wall_social_feed_alter(&$socialFeed) {
+    $socialFeed
+        ->addSource(social_wall_get_provider('facebook_page'), array('page_id' => 'your_page_id'))
+        ->addSource(social_wall_get_provider('twitter_search_api'), array('query' => 'your_search'))
+    ;
+}
