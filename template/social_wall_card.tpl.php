d56cdc8f6c3d33d2bb4e7603a13bd3304eae01cb
Paul-Émile MINY <paulemile.miny@gmail.com>
2015-07-06 11:13:11 +0200

    wip on templating

--- template/social_wall_card.tpl.php
+++ template/social_wall_card.tpl.php
@@ -1,9 +1,19 @@
-<blockquote>
-<header>
-<time datetime="<?php echo $post->getCreatedAt()->format('Y-m-d H:i:s') ?>"><?php echo format_date($post->getCreatedAt()->format('U')) ?></time>
-    <p><?php echo $post->getAuthor()->getName() ?></p>
-</header>
-<p>
-<?php echo $post->getMessage() ?>
-</p>
-</blockquote>
+<article class="social-card social-card-<?php echo $type ?>">
+    <header>
+        <div class="pull-left">
+            <a href="#">
+                <img class="media-object" src="<?php echo $post->getAuthor()->getProfilePicture()->getUrl() ?>"></img>
+            </a>
+        </div>
+        <div>
+            <p><?php echo $post->getAuthor()->getName() ?></p>
+            <time datetime="<?php echo $post->getCreatedAt()->format('Y-m-d H:i:s') ?>"><?php echo format_date($post->getCreatedAt()->format('U')) ?></time>
+        </div>
+    </header>
+    <blockquote>
+        <?php echo $post->getMessage() ?>
+    </blockquote>
+    <?php foreach($post->getMedias() as $media): ?>
+        <img class="img-responsive" src="<?php echo $media->getUrl() ?>"></img>
+    <?php endforeach ?>
+</article>
