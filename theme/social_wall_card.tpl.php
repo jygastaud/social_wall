d2f700a814829e58db0ceb076547d379cc6269a5
Juliette Girin <juliette.girin@lanetscouade.com>
2015-07-08 15:06:27 +0200

    styling

--- theme/social_wall_card.tpl.php
+++ theme/social_wall_card.tpl.php
@@ -1,5 +1,5 @@
 <article class="<?php echo $classes ?>">
-    <header class="clearfix">
+    <header class="clearfix social-wall-card__header">
         <div class="social-wall-card__icon-wrap">
             <svg class="icon-social-feed icon-social-feed--<?php echo $type ?>" width="32px" height="32px">
                 <use xlink:href="#<?php echo $type ?>" />
@@ -16,23 +16,28 @@
             </div>
             <div>
                 <p><?php echo $post->getAuthor()->getName() ?></p>
-                <time datetime="<?php echo $post->getCreatedAt()->format('Y-m-d H:i:s') ?>"><?php echo format_date($post->getCreatedAt()->format('U')) ?></time>
             </div>
         </div>
     </header>
     <blockquote class="social-wall-card__msg">
         <?php echo $post->getMessage() ?>
     </blockquote>
-    <?php foreach($post->getMedias() as $media): ?>
-        <a href="<?php echo $media->getLink() ?>">
-        <?php echo theme('imagecache_external', array(
-            'path'       => $media->getUrl(),
-            'style_name' => 'sw_post_picture',
-            'attributes' => array(
-                'class' => array('img-responsive')
-            )
-        )) ?>
-        </a>
-    <?php endforeach ?>
+    <div class="social-wall-card__illu">
+        <?php foreach($post->getMedias() as $media): ?>
+            <a href="<?php echo $media->getLink() ?>">
+            <?php echo theme('imagecache_external', array(
+                'path'       => $media->getUrl(),
+                'style_name' => 'sw_post_picture',
+                'attributes' => array(
+                    'class' => array('img-responsive')
+                )
+            )) ?>
+            </a>
+        <?php endforeach ?>
+    </div>
+
+    <footer class="social-wall-card__footer">
+        <time datetime="<?php echo $post->getCreatedAt()->format('Y-m-d H:i:s') ?>"><?php echo format_date($post->getCreatedAt()->format('U')) ?></time>
+    </footer>
 </article>
 
