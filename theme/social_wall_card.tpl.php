95c98b225886d59cb52d5aa812ddad71323b4c2d
Juliette Girin <juliette.girin@lanetscouade.com>
2015-07-08 12:14:21 +0200

    basic styling

--- theme/social_wall_card.tpl.php
+++ theme/social_wall_card.tpl.php
@@ -1,24 +1,27 @@
 <article class="<?php echo $classes ?>">
-    <header>
-        <div class="icon-wrapper">
-            <svg class="icon social-card-icon-type">
+    <header class="clearfix">
+        <div class="social-wall-card__icon-wrap">
+            <svg class="icon-social-feed icon-social-feed--<?php echo $type ?>" width="32px" height="32px">
                 <use xlink:href="#<?php echo $type ?>" />
             </svg>
         </div>
-        <div class="pull-left">
-            <a href="#">
-                <?php echo theme('imagecache_external', array(
-                    'path'       => $post->getAuthor()->getProfilePicture()->getUrl(),
-                    'style_name' => 'sw_profile_picture'
-                )) ?>
-            </a>
-        </div>
-        <div>
-            <p><?php echo $post->getAuthor()->getName() ?></p>
-            <time datetime="<?php echo $post->getCreatedAt()->format('Y-m-d H:i:s') ?>"><?php echo format_date($post->getCreatedAt()->format('U')) ?></time>
+
+        <div class="social-wall-card__account">
+            <div>
+                <a href="#">
+                    <?php echo theme('imagecache_external', array(
+                        'path'       => $post->getAuthor()->getProfilePicture()->getUrl(),
+                        'style_name' => 'sw_profile_picture'
+                    )) ?>
+                </a>
+            </div>
+            <div>
+                <p><?php echo $post->getAuthor()->getName() ?></p>
+                <time datetime="<?php echo $post->getCreatedAt()->format('Y-m-d H:i:s') ?>"><?php echo format_date($post->getCreatedAt()->format('U')) ?></time>
+            </div>
         </div>
     </header>
-    <blockquote>
+    <blockquote class="social-wall-card__msg">
         <?php echo $post->getMessage() ?>
     </blockquote>
     <?php foreach($post->getMedias() as $media): ?>
--- theme/social_wall_feed.tpl.php
+++ theme/social_wall_feed.tpl.php
@@ -1,5 +1,7 @@
-<article class="<?php echo $classes ?>" data-columns>
-    <?php foreach($feed as $post): ?>
-        <?php echo theme('social_wall_card', array('post' => $post)) ?>
-    <?php endforeach ?>
-</article>
+<div class="row">
+    <section class="<?php echo $classes ?>" data-columns>
+        <?php foreach($feed as $post): ?>
+            <?php echo theme('social_wall_card', array('post' => $post)) ?>
+        <?php endforeach ?>
+    </section>
+</div>
